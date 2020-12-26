a08739e3df099f2c9b5280bdd764948da7018a1a
rafael <rafaelcmd@gmail.com>
2020-12-26 11:31:32 -0300

    Added func Fragment on video service

--- application/services/video_service.go
+++ application/services/video_service.go
@@ -5,6 +5,8 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
+
 	"cloud.google.com/go/storage"
 	"github.com/rafaelcmd/encoder-service/application/repositories"
 	"github.com/rafaelcmd/encoder-service/domain"
@@ -63,4 +65,33 @@ func (v *VideoService) Download(bucketName string) error {
 	log.Printf("video %v has been stored", v.Video.ID)
 
 	return nil
+}
+
+func (v *VideoService) Fragment() error {
+	err := os.Mkdir(os.Getenv("localStoragePath") + "/" + v.Video.ID, os.ModePerm)
+
+	if err != nil {
+		return err
+	}
+
+	source := os.Getenv("localStoragePath") + "/" + v.Video.ID + ".mp4"
+	target := os.Getenv("localStoragePath") + "/" + v.Video.ID + ".frag"
+
+	cmd := exec.Command("mp4fragment", source, target)
+	
+	output, err := cmd.CombinedOutput()
+
+	if err != nil {
+		return err
+	}
+
+	printOutput(output)
+
+	return nil
+}
+
+func printOutput (out []byte) {
+	if len(out) > 0 {
+		log.Printf("=====> Output: %s\n", string(out))
+	}
 }
