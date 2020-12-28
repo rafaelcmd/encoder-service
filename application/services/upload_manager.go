1c38500b0a3e1a2444dc609d60caa7f50dbcde8a
rafael <rafaelcmd@gmail.com>
2020-12-28 12:04:22 -0300

    Added functions ProcessUpload and uploadWorker to the upload manager and created upload manager teste

--- application/services/upload_manager.go
+++ application/services/upload_manager.go
@@ -2,10 +2,11 @@ package services
 
 import (
 	"context"
-	"errors"
 	"io"
+	"log"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
 
 	"cloud.google.com/go/storage"
@@ -61,6 +62,59 @@ func (vu *VideoUpload) loadPaths() error {
 	}
 
 	return nil
+}
+
+func (vu *VideoUpload) ProcessUpload(concurrency int, doneUpload chan string) error {
+	in := make(chan int, runtime.NumCPU())
+	returnChannel := make(chan string)
+
+	err := vu.loadPaths()
+
+	if err != nil {
+		return err
+	}
+
+	uploadClient, ctx, err := getClientUpload()
+
+	if err != nil {
+		return err
+	}
+	
+	for process := 0; process < concurrency; process++ {
+		go vu.uploadWorker(in, returnChannel, uploadClient, ctx)
+	}
+
+	go func() {
+		for x:= 0; x < len(vu.Paths); x++ {
+			in <- x
+		}
+		close(in)
+	}()
+
+	for r := range returnChannel {
+		if r != "" {
+			doneUpload <- r
+			break
+		}
+	}
+
+	return nil
+}
+
+func (vu *VideoUpload) uploadWorker(in chan int, returnChannel chan string, uploadClient *storage.Client, ctx context.Context) {
+	for x := range in {
+		err := vu.UploadObject(vu.Paths[x], uploadClient, ctx)
+
+		if err != nil {
+			vu.Errors = append(vu.Errors, vu.Paths[x])
+			log.Printf("error during the upload: %v. Error: %v", vu.Paths[x], err)
+			returnChannel <- err.Error()
+		}
+
+		returnChannel <- ""
+	}
+
+	returnChannel <- "upload completed"
 }
 
 func getClientUpload() (*storage.Client, context.Context, error) {
---
+++ application/services/upload_manager_test.go
@@ -0,0 +1,49 @@
+package services_test
+
+import (
+	"log"
+	"os"
+	"testing"
+
+	"github.com/joho/godotenv"
+	"github.com/rafaelcmd/encoder-service/application/services"
+	"github.com/stretchr/testify/require"
+)
+
+func init() {
+	err := godotenv.Load("../../.env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+}
+
+func TestVideoServiceUpload(t *testing.T) {
+	video, repo := prepare()
+
+	videoService := services.NewVideoService()
+	videoService.Video = video
+	videoService.VideoRepository = repo
+
+	err := videoService.Download("encoder-service-bucket")
+	require.Nil(t, err)
+
+	err = videoService.Fragment()
+	require.Nil(t, err)
+
+	err = videoService.Encode()
+	require.Nil(t, err)
+
+	videoUpload := services.NewVideoUpload()
+	videoUpload.OutputBucket = "encoder-service-bucket"
+	videoUpload.VideoPath = os.Getenv("localStoragePath") + "/" + video.ID
+
+	doneUpload := make(chan string)
+	go videoUpload.ProcessUpload(50, doneUpload)
+
+	result := <- doneUpload
+	require.Equal(t, result, "upload completed")
+
+	err = videoService.Finish()
+	require.Nil(t, err)
+}
